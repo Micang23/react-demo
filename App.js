b5432570a823304c9ac7dcab583568c99baba731
Micang23 <medranomicah@gmail.com>
2020-07-20 14:48:26 +0800

    initial view

--- App.js
+++ App.js
@@ -3,36 +3,9 @@ import React from 'react';
 import {
   View,
 } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
 
-  return (
-    <View style={{
-      backgroundColor: "#aaa",
-      flex: 1,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-      alignContent: "center",
-      flexWrap: "wrap"
-    }}>
-      <View style={{
-        backgroundColor: "dodgerblue",
-        flexShrink: 1,
-        width: 400,
-        height: 100
-      }}/>
-      <View style={{
-        backgroundColor: "gold",
-        width: 100,
-        height: 100
-      }}/>
-      <View style={{
-        backgroundColor: "tomato",
-        width: 100,
-        height: 100
-      }}/>
-    </View>
-
-  );
+  return <WelcomeScreen />;
 }
